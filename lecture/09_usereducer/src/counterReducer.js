fd28dee26b126802e225a2d0df1d489bbde40631
kangaroo-helloworldlabs <kangaroo@helloworldlabs.kr>
2025-07-10 11:35:00 +0900

    update

---
+++ lecture/09_usereducer/src/counterReducer.js
@@ -0,0 +1,14 @@
+function counterReducer(state, action){
+  switch(action.type){
+    case 'INCREASE':
+      return {count: state.count + 1};
+    case 'DECREASE':
+      return {count: state.count - 1};
+    case 'RESET':
+      return {count: 0};
+    default:
+      return state;
+  }
+}
+
+export default counterReducer;
